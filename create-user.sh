fd60b7f44164a99a690e09e6fad23d6afdf52cb8
Froot <nikshvora@gmail.com>
2022-04-21 14:21:55 +0300

    Скрипт готов

--- create-user.sh
+++ create-user.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
-user_name="test_user"
-password="12345"
-servers=(
-    "192.168.4.145"
-)
+#!/usr/bin/expect
 
-useradd -G sudo -s /bin/bash $user_name
-echo -e "$password\n$password" | passwd -q $user_name 
+set user_name ""
+set user_password ""
+set admin_name ""
+set admin_password ""
+set servers {"192.168.232.11" "192.168.232.11"}
+
+foreach server $servers {
+    spawn ssh $admin_name@$server "sudo useradd -G sudo -s /bin/bash $user_name && sudo passwd -q $user_name"
+    expect "$admin_name@$server's password:"
+    send "$admin_password\r"
+    expect "Enter new UNIX password:"
+    send "$user_password\r"
+    expect "Retype new UNIX password:"
+    send "$user_password\r"
+    expect "#"
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y expect
+sudo chmod +x create-user.sh
+./create-user.sh
