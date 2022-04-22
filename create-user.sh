3a3c69e410b8dcef77fc1e80188e39ef3c7ba0bc
Froot <nikshvora@gmail.com>
2022-04-22 10:32:05 +0300

    Добавил распознание фингерпринта

--- create-user.sh
+++ create-user.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect
 
-set user_name "test_user"
-set user_password "test"
-set admin_name "n_shvora"
-set admin_password "fmapna7-s"
-set servers {"192.168.232.11" "192.168.232.12"}
+set user_name ""
+set user_password ""
+set admin_name ""
+set admin_password ""
+set servers {"" ""}
 
 foreach server $servers {
     spawn ssh $admin_name@$server "sudo useradd -g users -G sudo -s /bin/bash -d /home/$user_name -m $user_name && sudo passwd -q $user_name"
