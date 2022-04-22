75659ff7f23de81f22937dc524f27c33283cd6e1
Froot <nikshvora@gmail.com>
2022-04-22 10:31:21 +0300

    Добавил распознание фингерпринта

--- create-user.sh
+++ create-user.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/expect
 
-set user_name ""
-set user_password ""
-set admin_name ""
-set admin_password ""
-set servers {""}
+set user_name "test_user"
+set user_password "test"
+set admin_name "n_shvora"
+set admin_password "fmapna7-s"
+set servers {"192.168.232.11" "192.168.232.12"}
 
 foreach server $servers {
     spawn ssh $admin_name@$server "sudo useradd -g users -G sudo -s /bin/bash -d /home/$user_name -m $user_name && sudo passwd -q $user_name"
-    expect "$admin_name@$server's password:"
-    send "$admin_password\r"
-    expect "Enter new UNIX password:"
-    send "$user_password\r"
-    expect "Retype new UNIX password:"
-    send "$user_password\r"
-    expect "#"
+    expect {
+        "Are you sure you want to continue connecting" {send -- "yes\r"; exp_continue} 
+        "$admin_name@$server's password:" {send -- "$admin_password\r"; exp_continue}
+        "Enter new UNIX password:" {send -- "$user_password\r"; exp_continue}
+        "Retype new UNIX password:" {send -- "$user_password\r"; exp_continue}
+        "#"
+    }
 }
